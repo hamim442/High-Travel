43397d838240743b6a972fafd82f322ad08016ae
charles tucker <charlesrmtucker@gmail.com>
2024-08-09 16:21:58 -0700

    airlines database added

--- api/migrations/009_users_trips.py
+++ api/migrations/009_users_trips.py
@@ -1,6 +1,6 @@
 steps = [
     [
-        """"--sql
+        """--sql
         CREATE TABLE users_trips (
             user_id INTEGER NOT NULL REFERENCES users (id),
             trip_id INTEGER NOT NULL REFERENCES trips (id)
