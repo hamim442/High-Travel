137c2732946590892b243b1e4e130d9cc64a8389
Al Hamim <hamim442@icloud.com>
2024-08-29 12:42:58 -0700

    Working on travel plan

--- ghi/src/components/EditUserProfile.jsx
+++ ghi/src/components/EditUserProfile.jsx
@@ -4,10 +4,5 @@ function EditUserProfile(props) {
     return <p>This is going to get changed by json's code</p>
 }
 
-export default EditUserProfile
-
-function EditUserProfile() {
-    return <p>this will change later</p>
-}
 
 export default EditUserProfile
