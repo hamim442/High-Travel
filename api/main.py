113e975bbcd5e68660b2cd443cd78f1f984d5630
Al Hamim <hamim442@icloud.com>
2024-09-01 18:18:26 -0700

    get all, get by id and create airlines tested and succedded

--- api/main.py
+++ api/main.py
@@ -13,6 +13,13 @@ from routers import (
 
 import os
 
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+
+
 app = FastAPI()
 
 app.add_middleware(
--- api/tests/test_airline_router.py
+++ api/tests/test_airline_router.py
@@ -28,4 +28,60 @@ united_airliines = {
 
 class EmptyAirlineQueries:
     def get_all_airlines(self) -> list[Airline]:
+        return []
+        # return [Airline(**american_airlines), Airline(**delta_airlines)]
+
+
+class MockAirlinesQueries:
+    def get_all_airlines(self) -> list[Airline]:
         return [Airline(**american_airlines), Airline(**delta_airlines)]
+
+    def get_airline(self, id: int) -> Airline:
+        if id == 1:
+            return Airline(**american_airlines)
+        raise AirlineDoesNotExist(f"Airline {id} does not exist")
+
+    def create_airline(self, airline: AirlineRequest) -> Airline:
+        return Airline(id=3, **airline.model_dump())
+
+
+class TestAirlines(TestCase):
+    def test_get_all_airlines_empty(self):
+        # Arrange
+        app.dependency_overrides[AirlineQueries] = EmptyAirlineQueries
+        # Act
+        response = client.get("/api/airlines")
+        # Assert
+        assert response.status_code == 200
+        self.assertEqual(response.json(), [])
+        app.dependency_overrides = {}
+
+    def test_get_all_airlines(self):
+        app.dependency_overrides[AirlineQueries] = MockAirlinesQueries
+        response = client.get("/api/airlines")
+        assert response.status_code == 200
+        self.assertEqual(response.json(), [american_airlines, delta_airlines])
+        app.dependency_overrides = {}
+
+    def test_get_airlines_by_id(self):
+        app.dependency_overrides[AirlineQueries] = MockAirlinesQueries
+
+        response = client.get("/api/airlines/1")
+        assert response.status_code == 200
+        self.assertEqual(response.json(), american_airlines)
+
+        response = client.get("/api/airlines/987")
+        assert response.status_code == 404
+        self.assertEqual(response.json(), {"detail": "Airline not found."})
+        app.dependency_overrides = {}
+
+    def test_create_airline(self):
+        app.dependency_overrides[AirlineQueries] = MockAirlinesQueries
+
+        response = client.post("/api/airlines", json=united_airliines)
+        assert response.status_code == 200
+        created_airline = response.json()
+        self.assertEqual(created_airline["id"], 3)
+        self.assertEqual(created_airline["name"], united_airliines["name"])
+
+        app.dependency_overrides = {}
