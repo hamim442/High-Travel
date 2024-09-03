0c9a545d24113cfc630ae24c7c44a4538962662b
Al Hamim <alhamim223@gmail.com>
2024-09-03 16:56:14 +0000

    Edit accommodation

--- api/models/accommodations.py
+++ api/models/accommodations.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 
 class Accommodation(BaseModel):
@@ -35,3 +36,20 @@ class AccommodationRequest(BaseModel):
     total_price: float
     notes: str | None
     trip_id: int
+
+
+class AccommodationUpdate(BaseModel):
+    stays_id: Optional[int] | None
+    address: Optional[str] = None
+    city: Optional[str] = None
+    state_province: Optional[str] = None
+    zip_code: Optional[str] = None
+    country: Optional[str] = None
+    phone: Optional[str] = None
+    email: Optional[str] = None
+    check_in_date: Optional[datetime] = None
+    check_out_date: Optional[datetime] = None
+    number_of_guests: Optional[int] | None
+    total_price: Optional[float] = None
+    notes: Optional[str] = None
+    trip_id: Optional[int] | None
